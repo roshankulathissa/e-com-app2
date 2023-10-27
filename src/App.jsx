1b86f1eef97e00da0d66b57db6342450c945f63f
Roshan Madumadawa <roshankulathissa@gmail.com>
2023-10-27 08:58:11 +0530

    testng

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+import React from 'react';
+import AppRouter from "./Routers/AppRouter";
+
+function App()  {
+  return (
+    <AppRouter/>
+  );
+}
+
+export default App;  
+ 
